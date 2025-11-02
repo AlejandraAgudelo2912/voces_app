6832ec54a60d228a8558b74a95ee39cd55b32d21
LAPTOP-1DQNJ8L3\aleja <339210@alu.murciaeduca.es>
2025-11-02 18:24:03 +0100

    fix

--- guardar.php
+++ guardar.php
@@ -1,4 +1,17 @@
 <?php
+
+$dir = __DIR__ . '/datos';
+if (!is_dir($dir)) {
+    mkdir($dir, 0777, true);
+}
+chmod($dir, 0777);
+
+$archivo = $dir . '/mensajes.json';
+if (!file_exists($archivo)) {
+    file_put_contents($archivo, '[]');
+    chmod($archivo, 0666);
+}
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && !empty($_POST['mensaje'])) {
     $archivo = 'datos/mensajes.json';
     $mensaje = strip_tags($_POST['mensaje']);
